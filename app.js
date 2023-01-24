7fa1d63d2b0934f2b09d97ff4e408ce073a28a57
Kishan Pareek <kishan25pareek@gmail.com>
2023-01-24 21:51:47 +0530

    First Commit

---
+++ app.js
@@ -0,0 +1,75 @@
+const express = require("express");
+
+const bodyParser = require("body-parser");
+
+const  requestModule = require("request");
+
+const app = express();
+
+const https = require("https");
+
+app.use(express.static("public"));
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Server is listening on port 3000");
+});
+
+app.get("/", function(req, res){
+  res.sendFile(__dirname +"/signup.html");
+});
+
+// c21d63a09047315a56bde13b3cf3f723-us13->API Key
+ // 28102cdfbc->List ID
+app.post("/", function(req, res){
+  const firstName = req.body.fName;
+  const lastName = req.body.lName;
+  const email = req.body.email;
+
+  console.log(firstName, lastName, email);
+
+  const data = {
+    members: [
+      {
+        email_address: email,
+        status: "subscribed",
+        merge_fields: {
+          FNAME: firstName,
+          LNAME: lastName,
+        }
+
+      }
+    ]
+  };
+
+  var jsonData = JSON.stringify(data);
+  const url = "https://us13.api.mailchimp.com/3.0/lists/28102cdfbc";
+  const options = {
+    method: "POST",
+    auth: "kishan:c21d63a09047315a56bde13b3cf3f723-us14"
+  }
+  const request = https.request(url, options, function(response) {
+      if (response.statusCode >= 200 && response.statusCode < 300) {
+        res.sendFile(__dirname + "/success.html");
+          response.on("data", function(data) {
+              console.log(JSON.parse(data));
+          });
+      } else {
+        res.sendFile(__dirname +"/failure.html")
+          console.log(`statusCode: ${response.statusCode}`);
+          console.log(`statusMessage: ${response.statusMessage}`);
+      }
+  });
+
+  request.on('error', function(error) {
+      console.log(error);
+  });
+
+  request.write(jsonData);
+  request.end();
+});
+
+app.post("/failure", function(req, res){
+  res.redirect("/");
+})
